bd13cc1ad3da41875b571a5e8816f564f295bee0
Ian Wright <ipwright+dev@gmail.com>
2019-08-20 23:59:41 +0100

    working :)

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,49 @@
-var WebpackNotifierPlugin = require("webpack-notifier");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: ['babel-polyfill', "./src/client/registration/index.js"],
-    output: {
-        path: __dirname + "/src/client/builds/",
-        filename: "registration-bundle.js"
-    },
-    module: {
-        rules: [
-            { test: /\.css$/, loader: "style-loader!css-loader" },
-            { test: /\.jsx?$/, loader: "babel-loader" }
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
         ]
-    },
-    plugins: [
-        new WebpackNotifierPlugin(),
-    ],
-    devtool: "source-map"
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
 };
+
+// var WebpackNotifierPlugin = require("webpack-notifier");
+
+// module.exports = {
+//     entry: ['babel-polyfill', "./src/client/registration/index.js"],
+//     output: {
+//         path: __dirname + "/src/client/builds/",
+//         filename: "registration-bundle.js"
+//     },
+//     module: {
+//         rules: [
+//             { test: /\.css$/, loader: "style-loader!css-loader" },
+//             { test: /\.jsx?$/, loader: "babel-loader" }
+//         ]
+//     },
+//     plugins: [
+//         new WebpackNotifierPlugin(),
+//     ],
+//     devtool: "source-map"
+// };
